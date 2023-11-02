269c3b6db077e5c64ed578851650ae024169d03f
Cameron <camwebbyhalsall3@gmail.com>
2023-11-02 12:50:40 +0000

    Add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
 import "@/styles/globals.css";
 
 const queryClient = new QueryClient();
 
+const pjs = Plus_Jakarta_Sans({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Component className={pjs.className} {...pageProps} />
     </QueryClientProvider>
   );
 };
